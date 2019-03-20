160ccfe74b9a78755d071a5b7b1eaed1dd940d9c
Daniel Alexander Tello Gaete <dtellogaete@gmail.com>
2019-03-20 00:28:03 -0300

    2

--- web_personal/web_personal/settings.py
+++ web_personal/web_personal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'web_personal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'personal',
+        'USER': 'postgres',
+        'PASSWORD':'solver33',
+        'HOST':'localhost',
+        'PORT':5432,
     }
 }
 
